31c975237ee1bcc4e2afb3172a2f0fdc606f7ab2
Junior Maia <maiajr2394@gmail.com>
2023-03-29 16:56:36 -0300

    test, list

--- app/lib/home/home_page.dart
+++ app/lib/home/home_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:listinha/home/widget/custom_drawe.dart';
+import 'package:listinha/home/widget/task_card.dart';
+import 'package:listinha/src/shared/services/reaml/models/task_model.dart';
 import 'package:listinha/src/shared/themes/widgets/user_image_button.dart';
+import 'package:realm/realm.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -33,31 +34,64 @@ class _HomePageState extends State<HomePage> {
         label: const Text('Nova Lista'),
       ),
       body: Center(
-        child: Column(
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: SegmentedButton<int>(
-                segments: const [
-                  ButtonSegment(
-                    value: 0,
-                    label: Text('Todos'),
-                  ),
-                  ButtonSegment(
-                    value: 1,
-                    label: Text('Pendentes'),
-                  ),
-                  ButtonSegment(
-                    value: 2,
-                    label: Text('Concluídas'),
-                  ),
-                  ButtonSegment(
-                    value: 3,
-                    label: Text('Desativadas'),
-                  ),
-                ],
-                selected: const {1},
-                onSelectionChanged: (values) {},
+            ListView.separated(
+              padding: const EdgeInsets.only(
+                top: 50,
+                left: 30,
+                right: 30,
+                bottom: 200,
+              ),
+              itemCount: 100,
+              itemBuilder: (_, index) {
+                final board = TaskBoard(
+                  Uuid.v4(),
+                  'Nova lista de tarefas 1',
+                  tasks: [
+                    Task(Uuid.v4(), '', completed: true),
+                    Task(Uuid.v4(), '', completed: true),
+                    Task(Uuid.v4(), '', completed: true),
+                    Task(Uuid.v4(), '', completed: true),
+                  ],
+                );
+                return TaskCard(
+                  board: board,
+                  height: 140,
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(
+                  height: 10,
+                );
+              },
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: SegmentedButton<int>(
+                  segments: const [
+                    ButtonSegment(
+                      value: 0,
+                      label: Text('Todos'),
+                    ),
+                    ButtonSegment(
+                      value: 1,
+                      label: Text('Pendentes'),
+                    ),
+                    ButtonSegment(
+                      value: 2,
+                      label: Text('Concluídas'),
+                    ),
+                    ButtonSegment(
+                      value: 3,
+                      label: Text('Desativadas'),
+                    ),
+                  ],
+                  selected: const {1},
+                  onSelectionChanged: (values) {},
+                ),
               ),
             )
           ],
